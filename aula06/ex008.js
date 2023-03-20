435017685e430dfe65374817789a01ad98da40f1
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-20 12:09:47 -0300

    ex08 , 09 e 10 creados

---
+++ aula06/ex008.js
@@ -0,0 +1,8 @@
+var vel = 101
+let max = 100
+console.log(`a velocidade de seus carro e  ${vel} por Hora`)
+if(vel >= max){//condicao simples
+    console.log(`Voce ultrapasou a velocidade permitida, isso levou a multarlo`)
+}
+console.log(`Digija sempre com cuidado`)
+
