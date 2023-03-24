63655d8aa4904f275b9d4e6c471878a0d4c001fc
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-24 12:43:26 -0300

    update do desafio 1

--- aula7ex/ex016/script.js
+++ aula7ex/ex016/script.js
@@ -3,8 +3,16 @@ var inicio = window.document.getElementById('ini')
 var fim = window.document.getElementById('ifim')
 var pas = window.document.getElementById ('ipassos')
 var res = window.document.getElementById('res')
-for(var con = inicio.value; con < fim.value; con = con + pas.value ){
-    res.innerText = co
+let ini = Number(inicio.value)
+let fi = Number(fim.value)
+let pass = Number(pas.value)
+con = ini
+if (con == 0 ){
+    window.alert('0 nao pode ser o inicio')
+}else{
+while( con < fi){
+    res.innerHTML += con 
+    con = con + pass
 }
-
+}
 }
