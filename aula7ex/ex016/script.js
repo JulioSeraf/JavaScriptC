43c9e3517b66a83469bf2d99fd2149e1a040cd93
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-24 11:38:52 -0300

    ex16 creado

--- aula7ex/ex016/script.js
+++ aula7ex/ex016/script.js
@@ -1,3 +1,10 @@
 function contar(){
-    
+var inicio = window.document.getElementById('ini')
+var fim = window.document.getElementById('ifim')
+var pas = window.document.getElementById ('ipassos')
+var res = window.document.getElementById('res')
+for(var con = inicio.value; con < fim.value; con = con + pas.value ){
+    res.innerText = conn
+}
+
 }
