41cf9f7dbe447dc0cbd77d498c492bd554cd0048
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-26 14:49:04 -0300

    desafio 1 revisadado

--- aula7ex/ex016/script.js
+++ aula7ex/ex016/script.js
@@ -6,21 +6,29 @@ var res = window.document.getElementById('res')
 let ini = Number(inicio.value)
 let fi = Number(fim.value)
 let pass = Number(pas.value)
-con = ini
 res.innerHTML = ''
-if (pass == 0){
+if (pass <= 0){
     window.alert('Nao se pode contar [0] em Passos, vamos considerar [1] ')
     pass = 1 
-    while( con <= fi){
-        res.innerText += (`${con} &#x1F449`) 
-        con = con + pass
-    }
-}else if (fi == 0){
+}
+if (fi == 0 && ini == 0){
     res.innerHTML = '[0] valor de contargem, impossivel contar'
 }else{
-while( con <= fi){
-    res.innerText += (`${con} &#x1F449`) 
-    con = con + pass
+    res.innerHTML = 'Contando:'
+    if ( ini < fi){
+        //contagem crecente 
+        con = ini
+        while( con <= fi){
+            res.innerHTML += `${con} \u{1F449}`
+            con = con + pass
+         }
+
+    }else if(ini > fi) {
+        //contagem Regressiva
+        for(con = ini; con > fi; con = con - pass){
+        res.innerHTML += `${con} \u{1f449}`
+        }
+    }  
 }
-}
+res.innerHTML += `\u{1F3C1}`
 }
