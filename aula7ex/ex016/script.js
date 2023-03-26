a3c62a036d0ad757ce23973777233ca23e639874
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-26 13:51:21 -0300

    desafio 1 de aluna 07 termindo

--- aula7ex/ex016/script.js
+++ aula7ex/ex016/script.js
@@ -7,11 +7,19 @@ let ini = Number(inicio.value)
 let fi = Number(fim.value)
 let pass = Number(pas.value)
 con = ini
-if (con == 0 ){
-    window.alert('0 nao pode ser o inicio')
+res.innerHTML = ''
+if (pass == 0){
+    window.alert('Nao se pode contar [0] em Passos, vamos considerar [1] ')
+    pass = 1 
+    while( con <= fi){
+        res.innerText += (`${con} &#x1F449`) 
+        con = con + pass
+    }
+}else if (fi == 0){
+    res.innerHTML = '[0] valor de contargem, impossivel contar'
 }else{
-while( con < fi){
-    res.innerHTML += con 
+while( con <= fi){
+    res.innerText += (`${con} &#x1F449`) 
     con = con + pass
 }
 }
