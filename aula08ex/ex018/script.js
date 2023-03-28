5e13e110e4aba6658c8db6abfdf898019c0fb92a
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-03-28 11:28:32 -0300

    atualizado desafio ex08

--- aula08ex/ex018/script.js
+++ aula08ex/ex018/script.js
@@ -1,19 +1,49 @@
-function add(){
-    var num = window.document.getElementById ('inum')
-    var addnumero = window.document.querySelector('select#iadnum')
-    var numero = Number(num.value)
-    if (numero == 0 || numero >100){
-        window.alert(`[ERROR], Numero nao corresponde!`)
-    }else{
-    var itens = document.createElement ('option')
-    itens.text = `Numero ${numero} Adicionado`
-    addnumero.appendChild(itens)
-    }
-    
+var num = window.document.getElementById('inum')
+let lista =  window.document.getElementById('iadnum')
+var res = window.document.querySelector('div#res')
+let valores = []
 
+function isNumero(n){
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else{
+        return false
+    }
 
 }
+function inlista(n,l){
+    if(l.indexOf(Number(n)) != -1){
+        return true 
+
+    }else{
+        return false
+
+    }
+
+}
+
+
+function add(){
+    if(isNumero(num.value) && !inlista(num.value, valores)){
+        valores.push(Number(num.value))
+        var itens = window.document.createElement('option')
+        lista.appendChild(itens)
+        itens.text = `Numero ${num.value} add`
+
+    }else{
+        window.alert('Numero nao corresponde ou ja se encontra registrasdo')
+    }
+
+}
+function soma(n){
+    var total = 0
+    for(con = 0; con < n; con++){
+        total += valores[con]       
+}
+return total
+}
 function fim(){
-    var res = window.document.getElementById('res')
-    res = itens
+    res.innerHTML = `Total de  Numeros cadastrado foram ${valores.length} <br>`
+    res.innerHTML += `Soma Total dos Numeros ${soma(valores.length)} <br> `
+    res.innerHTML += `Media dos valores`soma(valores.length)/ valores.length
 }
